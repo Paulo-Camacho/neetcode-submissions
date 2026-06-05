6a4ba92ae0d2ab8c3999731576ac50ee985d00c2
Paulo-Camacho <165230497+Paulo-Camacho@users.noreply.github.com>
2026-06-04 17:01:40 -0700

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.py
@@ -0,0 +1,43 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer = [0] * len(temperatures)
+        stack = []
+        # Start with an empty stack
+        for curr in range(len(temperatures)):
+            while stack and temperatures[curr] > temperatures[stack[-1]]:
+                # The answer at given day = the difference found
+                answer[stack[-1]] = curr - stack[-1]
+                stack.pop()
+            stack.append(curr)
+                
+            # if temperatures[curr] > temperatures[stack[-1]]:
+            #     answer[stack[-1]] = curr - stack[-1]
+            #     # Once it has been solved we do not need it in the stack. This just appends
+            #     stack.append(curr)
+            # print(f'curr{curr} stack{stack}')
+
+        print(f'This is answer {answer}')
+        return answer
+            
+                
+
+
+
+
+
+
+
+        # Let's do brute force for right now just to get our brain warmed up 06-01-26 4:14PM
+        # answer = []
+        # for curr in range(len(temperatures)):
+        #     curr_val = temperatures[left]
+        #     self.diff = 0
+        #     for right in range(curr + 1, len(temperatures)):
+        #         right_val = temperatures[right]
+        #         if right_val > curr_val:
+        #             self.diff = right - curr
+        #             break
+        #     answer.append(self.diff)
+        # print(answer)
+        # return answer 
+            
