f6eafe67a361d7ecce0cf6d4b0e12ad86d2bea2c
Paulo-Camacho <165230497+Paulo-Camacho@users.noreply.github.com>
2026-09-02 11:12:45 -0700

    Add: binary-search - submission-12

---
+++ Data Structures & Algorithms/binary-search/submission-12.py
@@ -0,0 +1,17 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # Remeber the idea of binary search is to move the bounderies given if the target is larger or smaller
+        # Moving the bounderies in such a way that we get rid of half of the unused data
+        # [0, 2] 2
+        l = 0
+        r = len(nums) - 1
+        while(l <= r):
+            mid = math.floor((r + l) // 2) 
+            finger = nums[mid]
+            if finger == target:
+                return mid 
+            elif finger < target:
+                l = mid + 1
+            elif finger > target:
+                r = mid - 1
+        return -1
